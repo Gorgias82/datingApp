02ccd2b293f8aa52aa040c911e1b52d340b886a4
Jorge Vicente Ramiro <jorge.vicente@mercanza.local>
2022-08-02 14:13:57 +0200

    section 10

--- back/API/Controllers/UsersController.cs
+++ back/API/Controllers/UsersController.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace API.Controllers
@@ -56,6 +57,22 @@ namespace API.Controllers
             //return _mapper.Map<MemberDto>(user);
             return await _userRepository.GetMemberAsync(username);
 
+        }
+
+        [HttpPut]
+        public async  Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            //devuelve el username del token
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            //mapea las propiedades del memberupdatedto al user
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
     }
 
